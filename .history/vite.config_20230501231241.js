32b374fae916b56a6ae528f82bc23519b4121c91
Wajahat Kanju <wajahat5ahmad@gmail.com>
2023-05-01 23:17:10 +0500

    minor tweaks

--- .history/vite.config_20230501231241.js
+++ .history/vite.config_20230501231241.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/openweaterapi_test",
+  base: "/open_weater_api",
   plugins: [react()],
 })
